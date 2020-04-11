ffb1c5cb205194f76325db89260b18e50806300a
Kamba1a <headcr@bk.ru>
2020-04-11 16:57:09 +0300

    Урок 1. ПЗ

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         List<EmployeeViewModel> _employees;
+        List<BookViewModel> _books;
 
         public HomeController()
         {
@@ -19,20 +20,38 @@ namespace WebStore.Controllers
             new EmployeeViewModel{Id=1, FirstName="Иван", LastName="Иванов", Patronymic="Иванович", Age=30, Position="Специалист"},
             new EmployeeViewModel{Id=2, FirstName="Петр", LastName="Петров", Patronymic="Петрович", Age=35, Position="Начальник отдела"}
             };
+            _books = new List<BookViewModel>{
+                new BookViewModel{Id=1, Title="Все о нашей вселенной", Author="Иванов", PagesNumber=300, Year=2017},
+                new BookViewModel{Id=2, Title="Жизнь на планете Земля", Author="Петров", PagesNumber=150, Year=2015}
+            };
         }
 
         //GET: /<controller>/
         public IActionResult Index()
         {
             //возвращает представление из Views-Home-Index.cshtml
+            return View();
+        }
+
+        //GET: /<controller>/employees
+        public IActionResult Employees()
+        {
             return View(_employees);
         }
 
-
-        //GET: /<controller>/details/{id}
+        //GET: /<controller>/employees/details/{id}
         public IActionResult Details(int id)
         {
             return View(_employees.FirstOrDefault(empl => empl.Id == id));
+        }
+
+        public IActionResult Books()
+        {
+            return View(_books);
+        }
+        public IActionResult BookDetails(int id)
+        {
+            return View(_books.FirstOrDefault(book => book.Id == id));
         }
     }
 }
