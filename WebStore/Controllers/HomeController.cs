d44da5c7eb90c7f49cce96ab073e508cde3b98ae
Kamba1a <headcr@bk.ru>
2020-04-11 16:13:31 +0300

    Add project files.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<EmployeeViewModel> _employees;
+
+        public HomeController()
+        {
+            _employees = new List<EmployeeViewModel> { 
+            new EmployeeViewModel{Id=1, FirstName="Иван", LastName="Иванов", Patronymic="Иванович", Age=30, Position="Специалист"},
+            new EmployeeViewModel{Id=2, FirstName="Петр", LastName="Петров", Patronymic="Петрович", Age=35, Position="Начальник отдела"}
+            };
+        }
+
+        //GET: /<controller>/
+        public IActionResult Index()
+        {
+            //возвращает представление из Views-Home-Index.cshtml
+            return View(_employees);
+        }
+
+
+        //GET: /<controller>/details/{id}
+        public IActionResult Details(int id)
+        {
+            return View(_employees.FirstOrDefault(empl => empl.Id == id));
+        }
+    }
+}
