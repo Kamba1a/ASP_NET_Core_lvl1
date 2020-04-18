6b233d342eb87cef07ce8e56a84799222950b87a
Kamba1a <headcr@bk.ru>
2020-04-18 23:05:16 +0300

    Урок 3. ПЗ

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -3,35 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Infrastructure.Services;
 using WebStore.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebStore.Controllers
 {
+    //[Example_SimpleActionFilter] //через атрибут можно подключить фильтр к целому контроллеру или отдельным экшн-методам
+    //[Route("users")] //пример маршрутизации атрибутами (путь будет не employee, а users)
     public class EmployeeController : Controller
     {
-        List<EmployeeViewModel> _employees;
+        IitemService<EmployeeViewModel> _employees;
 
-        public EmployeeController()
+        public EmployeeController(IitemService<EmployeeViewModel> employees)
         {
-            _employees = new List<EmployeeViewModel> { 
-            new EmployeeViewModel{Id=1, FirstName="Иван", LastName="Иванов", Patronymic="Иванович", Age=30, Position="Специалист"},
-            new EmployeeViewModel{Id=2, FirstName="Петр", LastName="Петров", Patronymic="Петрович", Age=35, Position="Начальник отдела"}
-            };
+            _employees = employees;
         }
 
-
+        //[Route("all")] //пример маршрутизации атрибутами (// GET: users/all)
         //GET: /<controller>/employees
         public IActionResult Employees()
         {
-            return View(_employees);
+            return View(_employees.GetAll());
         }
 
+        //[Route("{id}")] //пример маршрутизации атрибутами (// GET: /users/{id})
         //GET: /<controller>/employees/details/{id}
         public IActionResult Details(int id)
         {
-            return View(_employees.FirstOrDefault(empl => empl.Id == id));
+            return View(_employees.GetById(id));
+        }
+
+        //GET: /<controller>/employees/edit/{id?}
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue) return View(new EmployeeViewModel());
+            EmployeeViewModel employee = _employees.GetById(id.Value);
+            if (employee==null) return NotFound();
+            return View(employee);
+        }
+
+        //POST: /<controller>/employees/edit/
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel _employee)
+        {
+            EmployeeViewModel employee = _employees.GetById(_employee.Id);
+            if (employee == null) _employees.AddNew(_employee);
+            else
+            {
+                employee.FirstName = _employee.FirstName;
+                employee.LastName = _employee.LastName;
+                employee.Patronymic = _employee.Patronymic;
+                employee.Age = _employee.Age;
+                employee.Position = _employee.Position;
+            }
+            _employees.Commit();
+            return RedirectToAction("Employees");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _employees.Delete(id);
+            return RedirectToAction("Employees");
         }
     }
 }
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Infrastructure.Services;
+using WebStore.Models;
 
 namespace WebStore
 {
@@ -17,8 +20,19 @@ namespace WebStore
         public void ConfigureServices(IServiceCollection services)
         {
             //сюда добавляем сервисы, какие будем использовать
+
             services.AddMvc();
+            //(options => options.Filters.Add(new Example_SimpleActionFilter())) для добавления фильтра ко всем методам всех контроллеров
+
+            //Разрешение зависимости:
+            //services.AddSingleton<IService, InMemoryService>(); время жизни сервиса = времени жизни запущенной программы 
+            //services.AddScoped(); равно времени жизни http-запроса (до обновления/закрытия страницы)
+            //services.AddTransient(); - обновляется при каждом запросе
+
+            services.AddSingleton(typeof(IitemService<EmployeeViewModel>), typeof(InMemoryEmployeesService));
+            services.AddSingleton(typeof(IitemService<BookViewModel>), typeof(InMemoryBooksService));
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,6 +63,10 @@ namespace WebStore
                 //    await context.Response.WriteAsync("Hello World!");
                 //});
             });
+
+            //аналог app.UseEndpoints в одну строку (но без возможности подробно настроить маршруты по-умолчанию)
+            //app.UseMvcWithDefaultRoute();
+            //кроме того, в services.AddMvc тогда надо прописать options => (options.EnableEndpointRouting = false)
         }
     }
 }
