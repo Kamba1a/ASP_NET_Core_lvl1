237037089dcde45575c1810d3b813bcf8eb2169f
Kamba1a <headcr@bk.ru>
2020-04-21 22:16:52 +0300

    Урок 4. Добавлены классы для товаров и фильтрации товаров

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Shop()
+        IProductData _productData;
+
+        public CatalogController(IProductData productData)
         {
-            return View();
+            _productData = productData;
+        }
+
+        public IActionResult Shop(int? sectionId, int? brandId)
+        {
+            var products = _productData.GetProducts(new ProductFilter { BrandId = brandId, SectionId = sectionId });
+
+            var model = new CatalogViewModel
+            {
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+
+            return View(model);
         }
 
         public IActionResult ProductDetails()
--- WebStore/Infrastructure/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Infrastructure.Interfaces
 {
+    /// <summary>
+    /// Интерфейс для работы с товарами
+    /// </summary>
     public interface IProductData
     {
+        /// <summary>
+        /// Список секций
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<Section> GetSections();
 
+        /// <summary>
+        /// Список брендов
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<Brand> GetBrands();
+
+        /// <summary>
+        /// Список товаров
+        /// </summary>
+        /// <param name="filter">Фильтр товаров</param>
+        /// <returns></returns>
+        IEnumerable<Product> GetProducts(ProductFilter filter);
     }
 }
