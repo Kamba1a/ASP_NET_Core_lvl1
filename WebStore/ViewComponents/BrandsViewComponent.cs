d5559e50b16208c8525a88cc00514e98cc3590de
Kamba1a <headcr@bk.ru>
2020-04-22 23:02:29 +0300

    Урок 4. Реализован подсчет товаров в каждом бренде (с отображением рядом с именем бренда в левом меню)

--- WebStore/ViewComponents/BrandsViewComponent.cs
+++ WebStore/ViewComponents/BrandsViewComponent.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
 
 namespace WebStore.ViewComponents
 {
@@ -18,7 +19,31 @@ namespace WebStore.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View(_catalogData.GetAllBrands().OrderBy(brand => brand.Order));
+            return View(GetBrands());
+        }
+
+        IEnumerable<BrandViewModel> GetBrands()
+        {
+            List<BrandViewModel> brands = _catalogData.GetAllBrands().ToList();
+
+            foreach (BrandViewModel brand in brands)
+            {
+                brand.ProductCount = GetProductCount(brand.Id);
+            }
+
+            return brands.OrderBy(brand => brand.Order);
+        }
+
+        int GetProductCount(int brandId)
+        {
+            int productCount = 0;
+
+            foreach (ProductViewModel product in _catalogData.GetAllProducts())
+            {
+                if (product.BrandId == brandId) productCount = productCount + 1;
+            }
+
+            return productCount;
         }
     }
 }
