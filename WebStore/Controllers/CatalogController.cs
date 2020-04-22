a645253eba7622b9aa00d645c0cd4fa164fde3a5
Kamba1a <headcr@bk.ru>
2020-04-22 23:04:40 +0300

    Урок 4. Добавлен контроллер Catalog, перенесено представление Shop и частичные представления, поправлены ссылки в меню _Header

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        ICatalogData _catalogData;
+
+        public CatalogController(ICatalogData catalogData)
+        {
+            _catalogData = catalogData;
+        }
+
+        public IActionResult Shop(int? categoryId, int? brandId)
+        {
+            List<ProductViewModel> products = _catalogData.GetAllProducts().ToList();
+
+            if (categoryId.HasValue)
+            {
+                products = _catalogData.GetAllProducts()
+                    .Where(product => product.CategoryId == categoryId.Value)
+                    .ToList();
+            }
+            else if (brandId.HasValue)
+            {
+                products = _catalogData.GetAllProducts()
+                .Where(product => product.BrandId == brandId.Value)
+                .ToList();
+            }
+
+            return View(products.OrderBy(product => product.Order));
+        }
+    }
+}
