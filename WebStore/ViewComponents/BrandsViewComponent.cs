657b57b5fbe65908cd2d047402e4ee0af7b53b82
Kamba1a <headcr@bk.ru>
2020-04-22 17:49:42 +0300

    Урок 4. Добавлен ViewComponent для брендов и отображение брендов в левом меню

---
+++ WebStore/ViewComponents/BrandsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.ViewComponents
+{
+    public class BrandsViewComponent: ViewComponent
+    {
+        ICatalogData _catalogData;
+
+        public BrandsViewComponent(ICatalogData catalogData)
+        {
+            _catalogData = catalogData;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(_catalogData.GetAllBrands().OrderBy(brand => brand.Order));
+        }
+    }
+}
