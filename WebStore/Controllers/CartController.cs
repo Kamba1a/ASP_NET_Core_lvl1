b1c83017cc9f53febf114dd32d5799334e961e59
Olga Nazarova <headcr@bk.ru>
2020-05-02 20:48:00 +0300

    Урок 7. Добавлен контроллер для корзины и представление, поправлены ссылки, связанные с корзиной в других представлениях

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IActionResult Cart()
+        {
+            return View(_cartService.TransformCartToViewModel());
+        }
+
+        public IActionResult AddToCart(int productId, string returnUrl)
+        {
+            _cartService.AddToCart(productId);
+            return Redirect(returnUrl); //редирект на страницу, где был пользователь
+        }
+
+        public IActionResult RemoveAllProducts()
+        {
+            _cartService.RemoveAllProductsFromCart();
+            return RedirectToAction("Cart");
+        }
+
+        public IActionResult RemoveProduct(int productId)
+        {
+            _cartService.RemoveProductFromCart(productId);
+            return RedirectToAction("Cart");
+        }
+
+        public IActionResult DecrementProductQuantity(int productId)
+        {
+            _cartService.DecrementProductQuantity(productId);
+            return RedirectToAction("Cart");
+        }
+    }
+}
